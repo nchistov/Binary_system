457077fff2d5862935a881f9fa153d8adbc23db0
nchistov <nikola-chistov@mail.ru>
2021-12-21 11:57:44 +0300

    change method draw in game.py

--- game.py
+++ game.py
@@ -1,24 +1,28 @@
-import pygame as pg
 from button import Button
 
 
 class Game:
-    """Класс предназначен для управления экранами игры."""
-    def __init__(self, screen, score_table, rows, aims_results_table, check_button):
+    """Класс предназначеный для управления экранами игры."""
+    def __init__(self, screen, score_table, rows, aims_results_table, check_button, stats):
         self.screen = screen
         self.score_table = score_table
         self.rows = rows
         self.aims_results_table = aims_results_table
         self.check_button = check_button
+        self.stats = stats
 
     def draw(self):
-        self.score_table.draw_table()
+        match self.stats.game_state:
+            case 0:
+                pass
+            case 1:
+                self.score_table.draw_table()
 
-        for row in self.rows:
-            for button in row:
-                assert isinstance(button, Button)
-                button.draw_button()
+                for row in self.rows:
+                    for button in row:
+                        assert isinstance(button, Button)
+                        button.draw_button()
 
-        self.aims_results_table.show()
+                self.aims_results_table.show()
 
-        self.check_button.draw_button()
+                self.check_button.draw_button()
